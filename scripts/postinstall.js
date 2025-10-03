f549b1c2dbdbbbad67e2a2584e242f91e8dcc3a2
Andrew Mikofalvy <amikofalvy@gmail.com>
2025-10-03 13:13:41 -0700

    improve post install script to work cross platform (#23)

---
+++ scripts/postinstall.js
@@ -0,0 +1,21 @@
+// cross-platform postinstall
+import { execSync } from "node:child_process";
+
+const isVercel = process.env.VERCEL === "1";
+const skip = process.env.SKIP_APP_INSTALL === "1";
+
+if (isVercel && !skip) {
+	// Export the UI
+	execSync("inkeep dev --export --output-dir ./apps/manage-ui", {
+		stdio: "inherit",
+	});
+
+	// Install dependencies in manage-ui with SKIP_APP_INSTALL flag
+	execSync(
+		"pnpm -C apps/manage-ui install --no-frozen-lockfile --ignore-scripts",
+		{
+			stdio: "inherit",
+			env: { ...process.env, SKIP_APP_INSTALL: "1" },
+		},
+	);
+}
